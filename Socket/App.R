d2d33c6fcd8537390078db4bef1ecc9bfe8d9872
Andy31700 <dourel.andy@gmail.com>
2022-11-28 10:01:42 +0100

    Socket

---
+++ Socket/App.R
@@ -0,0 +1,72 @@
+library(shiny)
+library(shinyjs)
+closeAllConnections()
+
+# Connection to the socket
+host <- "127.0.0.1"
+port <- 6007
+con <- make.socket(host, port)
+writeLines(paste("-- Server --", "\n", "Host: ", host, "\n","Port: ", port))
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  useShinyjs(),
+  actionButton("go", label = "Go!"),
+  actionButton("stop", label = "Stop!"),
+  textOutput('status'),
+  uiOutput("image"),
+  
+  # Application title
+  titlePanel("test"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot"),
+      htmlOutput("showfile"),
+      
+      # Output: Tabset w/ plot, summary, and table
+      tabsetPanel(id = "tabs",
+                  tabPanel("Plot", plotOutput("ID1")),
+                  tabPanel("Summary", verbatimTextOutput("ID2")),
+                  tabPanel("Table", tableOutput("ID3"))
+      )
+    )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) { 
+  
+  observeEvent(input$tabs, {
+    
+    print(paste("You clicked tab:", input$tabs))
+    print(paste("The ID session is :", session$token))
+    msg <<- paste0("[",Sys.time(), "] You clicked tab : ", input$tabs, " | Your session ID is : ", session$token)
+    
+    #Send data
+    write.socket(con, msg)
+    
+    #Read the socket
+    statusMsg = read.socket(con, loop=TRUE)
+    print(statusMsg)
+  
+  output$showfile <- renderUI({
+    includeHTML("C:/Users/doure/Downloads/TTMéNOT/Rasat/index.html")
+  })
+  }
+  )
+}
+
+# Run the application 
+shinyApp(ui=ui, server=server)
---
+++ Socket/Server_Socket.py
@@ -0,0 +1,26 @@
+import socket
+import select
+import time
+
+#shiny::shinyAppFile("C:/Users/doure/Downloads/Rshiny-intro/Socket/App.R")
+
+# Creation of the socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('127.0.0.1', 6007))
+server_socket.listen()
+(client_socket, client_address) = server_socket.accept()
+time.sleep(10)
+while True:
+
+     #Receive the data from R
+    data = client_socket.recv(1024)
+    str = data.decode('ascii')
+    print(str)
+
+    # Send data to R
+    what_to_send = 'Hello ' + data.decode()
+    client_socket.send(what_to_send.encode())
+
+#Close the socket
+#client_socket.close()
+#server_socket.close()
